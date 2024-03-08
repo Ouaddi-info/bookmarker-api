838ff44ec5ad42df475a7611da0736cf5401d232
MUSTAPROAI <contact.mustapro@gmail.com>
2024-03-08 22:57:16 +0100

    Add flyway script migration to H2 and postgresql

---
+++ src/main/resources/db/migration/h2/V2__insert_sample_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO bookmarks(title,url,created_at) VALUES
+('How to remove docker','https:www.devoog.com/docker',CURRENT_TIMESTAMP()),
+('How to remove k8s','https:www.devoog.com/k8s',CURRENT_TIMESTAMP()),
+('How to add git','https:www.devoog.com/git',CURRENT_TIMESTAMP())
+;
