a4c16b89d4f227379c9ed517c7e95d2f02ff07c5
MUSTAPROAI <contact.mustapro@gmail.com>
2024-03-09 13:02:46 +0100

    Add DTO and Mapper

--- src/main/java/com/devooglabs/bookmarkerapi/api/BookmarkController.java
+++ src/main/java/com/devooglabs/bookmarkerapi/api/BookmarkController.java
@@ -1,15 +1,12 @@
 package com.devooglabs.bookmarkerapi.api;
 
-import com.devooglabs.bookmarkerapi.domain.Bookmark;
-import com.devooglabs.bookmarkerapi.domain.BookmarkDTO;
+import com.devooglabs.bookmarkerapi.domain.BookmarksDTO;
 import com.devooglabs.bookmarkerapi.domain.BookmarkService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/bookmarks")
@@ -18,7 +15,7 @@ public class BookmarkController {
     private final BookmarkService bookmarkService;
 
     @GetMapping
-    public BookmarkDTO getBookmarks(@RequestParam(name = "page",defaultValue = "0") Integer page){
+    public BookmarksDTO getBookmarks(@RequestParam(name = "page",defaultValue = "0") Integer page){
         return bookmarkService.getBookmarks(page);
     }
 }
--- src/main/java/com/devooglabs/bookmarkerapi/domain/BookmarkService.java
+++ src/main/java/com/devooglabs/bookmarkerapi/domain/BookmarkService.java
@@ -3,23 +3,24 @@ package com.devooglabs.bookmarkerapi.domain;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.annotation.ReadOnlyProperty;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class BookmarkService {
     private final BookmarkRepository repository;
+    private final BookmarkMapper bookmarkMapper;
 
     @ReadOnlyProperty
-    public BookmarkDTO getBookmarks(Integer page){
+    public BookmarksDTO getBookmarks(Integer page){
         int pageNo = page < 1 ? 0 : page -1 ;
         Pageable pageable= PageRequest.of(pageNo,10, Sort.Direction.DESC,"createdAt");
-        return new BookmarkDTO(repository.findAll(pageable)) ;
+        Page<BookmarkDTO> bookmarkDTOPage = repository.findBookmarks(pageable);
+        return new BookmarksDTO(bookmarkDTOPage) ;
     }
 }
